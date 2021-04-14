efb3ed7042354c59b43c6085b6ca59272d090bfc
Rita-Khalaf <rita99.khalaf@gmail.com>
2021-04-14 23:09:59 +0300

    first commit

---
+++ routes/equipmentController.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Equipment = require('../schemas/equipment');
+const EquipmentType = require("../schemas/equipmentType");
+//const {ObjectId} = require("bson").ObjectID;
+let ObjectId = require('mongodb').ObjectID;
+
+const equipmentRouter = express.Router();
+
+const mqtt = require('mqtt');
+const topic = 'topic';
+
+//connect to mqtt
+var client = mqtt.connect('mqtt://212.98.137.194:1883',{username:'iotleb',password:'iotleb'});
+client.on('connect', () => {
+   console.log('connected to mqtt');
+});
+
+equipmentRouter.route('/')
+    .get((req,res,next) => {
+        Equipment.find({})
+            .populate('equipmentTypeId')
+            .then((equipment) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(equipment);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })
+    .post((req, res, next) => {
+        req.body.equipmentTypeId = new ObjectId(req.body.equipmentTypeId);
+        Equipment.create(req.body)
+            //.populate('equipmentTypeId')
+            .then((equipment) => {
+                console.log('Equipment Created ', equipment);
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(equipment);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })
+    .delete( (req, res, next) => {
+        Equipment.remove({})
+            .then((resp) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(resp);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    });
+
+equipmentRouter.route('/:equipmentId')
+    .get((req,res,next) => {
+        Equipment.findById(req.params.equipmentId)
+            .populate('equipmentTypeId')
+            .then((equipment) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(equipment);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })
+    .post((req, res, next) => {
+        res.statusCode = 403;
+        res.end('POST operation not supported on /equipment/'+ req.params.equipmentId);
+    })
+    .put((req, res, next) => {
+        Equipment.findByIdAndUpdate(req.params.equipmentId, {
+            $set: req.body
+        }, { new: true })
+            .populate('equipmentTypeId')
+            .then((equipment) => {
+                var message = {
+                    equipmentId: req.params.equipmentId,
+                    networkAddress: equipment.networkAddress,
+                    Content: req.body
+                    //TODO: equipment
+                }
+                client.publish(topic, JSON.stringify(message));
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(equipment);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })
+    .delete((req, res, next) => {
+        Equipment.findByIdAndRemove(req.params.equipmentId)
+            .then((resp) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(resp);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    });
+
+module.exports = equipmentRouter;
---
+++ subscriber.js
@@ -0,0 +1,25 @@
+var mqtt = require('mqtt');
+var client = mqtt.connect('mqtt://212.98.137.194:1883',{username:'iotleb',password:'iotleb'});
+
+const topic = 'topic';
+const topic1 = 'LAMP';
+const topic2 = 'HEATER';
+
+const publishMessage = (topic, message) => {
+    client.publish(topic, message);
+} 
+
+client.on('connect', () => {
+    
+    client.subscribe(topic);
+    //client.subscribe(topic2);
+    
+});
+
+const handleMessage = message => {
+    console.log(message.value);
+}
+
+client.on('message', (topic, message) => {
+    console.log("received: " + message.toString());
+});
